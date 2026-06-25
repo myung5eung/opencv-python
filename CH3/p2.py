5b970be4fd83cf4a595bf9ef58e7e74717fc6507
myung5eung <gunmyeong17@gmail.com>
2026-06-25 14:51:14 +0900

    Update p2.py

--- CH3/p2.py
+++ CH3/p2.py
@@ -1,5 +1,11 @@
 import numpy as np
 
-A = np.array([[1,2,3],[10,-1,-5],[6,7,5]])
-r=np.linalg.matrix_power(A,3)-5*np.linalg.matrix_power(A,2)-4*A-98*np.eye(3)
-print(r)
+A = np.array([[1,2,3],[-1,3,-10]])
+B = np.array([[2,-1],[3,1],[0,5]])
+AB=np.zeros((A.shape[0],B.shape[1]))
+
+for i in range(A.shape[0]):
+    for j in range(B.shape[1]):
+        for k in range(B.shape[0]):
+            AB[i][j]+=A[i][k]*B[k][j]
+print(AB)
