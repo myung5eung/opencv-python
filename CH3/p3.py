5dd678c58dafac07b3e502b1d94f881363af4241
myung5eung <gunmyeong17@gmail.com>
2026-06-25 13:34:27 +0900

    Update p3.py

--- CH3/p3.py
+++ CH3/p3.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 A = np.array([[1,2,3],[10,-1,-5],[6,7,5]])
-r=np.linalg.matrix_power(A,3)-5*np.linalg.matrix_power(A,2)-4*A-98*np.eye(3)
+i=np.eye(A.shape[0],A.shape[1])
+r=np.linalg.matrix_power(A,3)-5*np.linalg.matrix_power(A,2)-4*A-98*i
 print(r)
