24f143eb060e03a1d61e2bd39b5d4a5797fa4ffd
myung5eung <gunmyeong17@gmail.com>
2026-06-26 14:07:52 +0900

    Create p3.py

---
+++ CH4/p3.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+def onMouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"좌표: ({x},{y}), 화소값(B,R,G): ({param[x,y][0],param[x,y][1],param[x,y][2]})")
+
+image =  cv2.imread('lenna.bmp',cv2.IMREAD_COLOR)
+if image is None:
+    raise Exception("파일 읽기 오류")
+
+cv2.imshow('Mouse Event', image)
+cv2.setMouseCallback('Mouse Event', onMouse, image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
