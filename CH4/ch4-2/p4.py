3a161429ab9d880c5935dc3e7eb19a0e59daf07a
myung5eung <gunmyeong17@gmail.com>
2026-06-29 16:15:20 +0900

    Update p4.py

--- CH4/ch4-2/p4.py
+++ CH4/ch4-2/p4.py
@@ -1,1 +1,28 @@
+import numpy as np
+import cv2
+image = np.zeros((300, 300, 3), np.uint8)
 
+cnt = 0
+running = False
+
+while True:
+    image[:] = 255
+    cv2.putText(image, str(cnt),(120, 170),cv2.FONT_HERSHEY_SIMPLEX,3, (0, 0, 0), 5)
+    cv2.imshow('watch', image)
+
+    key = cv2.waitKey(1000)
+
+    if key == ord('s'):
+        running = True
+    elif key == ord('t'):
+        running = False
+    elif key == ord('r'):
+        cnt = 0
+        running = False
+    elif key == ord('q'):
+        break
+
+    if running:
+        cnt += 1
+
+cv2.destroyAllWindows()
