7f084a25477319962ab2f77a50b6361a293f8d5f
myung5eung <gunmyeong17@gmail.com>
2026-06-29 16:10:53 +0900

    Update p1.py

--- CH4/ch4-2/p1.py
+++ CH4/ch4-2/p1.py
@@ -1,24 +1,24 @@
 import numpy as np
 import cv2
-# image1 = np.zeros((400, 400), np.uint8)
-# image1[:] = 255    
+image1 = np.zeros((400, 400), np.uint8)
+image1[:] = 255    
 
-# image2 = np.zeros((400, 400), np.uint8)
-# image2[:] = 255    
-# h, w = image1.shape
-# pt1 = (w // 4, h // 4)
-# pt2 = (w * 3 // 4, h * 3 // 4)
-# cv2.rectangle(image1, pt1, pt2, 0, 1)
-# cv2.line(image1, pt1, pt2, 0, 1)
-# cv2.line(image1, (w // 4, h * 3 // 4), (w * 3 // 4, h // 4), 0, 1)
+image2 = np.zeros((400, 400), np.uint8)
+image2[:] = 255    
+h, w = image1.shape
+pt1 = (w // 4, h // 4)
+pt2 = (w * 3 // 4, h * 3 // 4)
+cv2.rectangle(image1, pt1, pt2, 0, 1)
+cv2.line(image1, pt1, pt2, 0, 1)
+cv2.line(image1, (w // 4, h * 3 // 4), (w * 3 // 4, h // 4), 0, 1)
 
-# cv2.rectangle(image2, pt1, pt2, 0, 1)
-# center = (w // 2, h // 2)
-# cv2.circle(image2, center, w // 4, 0)
-# cv2.imshow("src1", image1)
-# cv2.imshow("src2", image2)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.rectangle(image2, pt1, pt2, 0, 1)
+center = (w // 2, h // 2)
+cv2.circle(image2, center, w // 4, 0)
+cv2.imshow("src1", image1)
+cv2.imshow("src2", image2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # image = cv2.imread('lenna.bmp',cv2.IMREAD_COLOR)
 # if image is None:
