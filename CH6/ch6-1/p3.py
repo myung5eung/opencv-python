92423f86bebf642511c724f639998657ab430f16
myung5eung <gunmyeong17@gmail.com>
2026-07-02 01:02:01 +0900

    Update p3.py

--- CH6/ch6-1/p3.py
+++ CH6/ch6-1/p3.py
@@ -3,27 +3,27 @@ import cv2
 class LennaG:
     def __init__(self, image):
         self.image = image
-        self.b = 0
-        self.mode = 0
+        self.b = 0 #밝기 변화량
+        self.mode = 0 #트랙바 
         cv2.imshow("dst", self.image)
-        cv2.createTrackbar("mode", "dst", 0, 1, self.onChange)
-        cv2.setMouseCallback("dst", self.onMouse)
+        cv2.createTrackbar("mode", "dst", 0, 1, self.onChange) #창에 0~1 범위인 트랙바를 생성하고 값이 변하면 onChange 실행
+        cv2.setMouseCallback("dst", self.onMouse) #dst창에서 마우스 이벤트가 발생하면 onMouse 실행
 
     def onChange(self, value):
-        self.mode = value
+        self.mode = value #트랙바 값을 mode에 저장
 
     def onMouse(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            if self.mode == 0:
+            if self.mode == 0: 
                 self.b += 10
             else:
                 self.b -= 10
             if self.b >= 0:
-                dst = cv2.add(self.image, self.b)
+                dst = cv2.add(self.image, self.b)  #image의 모든 픽셀값에 밝기 변화량을 더함
             else:
-                dst = cv2.subtract(self.image, -self.b)
+                dst = cv2.subtract(self.image, -self.b) #뺌
 
-            cv2.imshow("dst", dst)
+            cv2.imshow("dst", dst)  #밝기가 조절된 결과 영상을 화면에 출력함
 
 
 image = cv2.imread("lenna.bmp", cv2.IMREAD_GRAYSCALE)
