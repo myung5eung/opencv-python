8e71cbd946e10e1ce7e8c24bd7b47202e74e8e2c
myung5eung <gunmyeong17@gmail.com>
2026-07-02 00:57:12 +0900

    Update p3.py

--- CH6/ch6-1/p3.py
+++ CH6/ch6-1/p3.py
@@ -1,39 +1,36 @@
 import cv2
 
-class lennag:
+class LennaG:
     def __init__(self, image):
         self.image = image
         self.b = 0
+        self.mode = 0
         cv2.imshow("dst", self.image)
-        cv2.createTrackbar("bar", "dst", 0, 1, self.onChange)
+        cv2.createTrackbar("mode", "dst", 0, 1, self.onChange)
         cv2.setMouseCallback("dst", self.onMouse)
 
     def onChange(self, value):
-        self.show()
+        self.mode = value
 
     def onMouse(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            mode = cv2.getTrackbarPos("bar", "dst")
-            if mode == 0:
+            if self.mode == 0:
                 self.b += 10
             else:
                 self.b -= 10
-            self.b = max(-255, min(255, self.b))
-            self.show()
+            if self.b >= 0:
+                dst = cv2.add(self.image, self.b)
+            else:
+                dst = cv2.subtract(self.image, -self.b)
 
-    def show(self):
-        if self.b >= 0:
-            dst = cv2.add(self.image, self.b)
-        else:
-            dst = cv2.subtract(self.image, -self.b)
-
-        cv2.imshow("dst", dst)
+            cv2.imshow("dst", dst)
 
 
 image = cv2.imread("lenna.bmp", cv2.IMREAD_GRAYSCALE)
 if image is None:
     raise Exception("파일 읽기 오류")
-a = lennag(image)
+
+app = LennaG(image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
