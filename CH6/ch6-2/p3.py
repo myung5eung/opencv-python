662bdc5df4cad7edc252eb8f81bdc5f1019d5d29
myung5eung <gunmyeong17@gmail.com>
2026-07-01 16:16:20 +0900

    Update p3.py

--- CH6/ch6-2/p3.py
+++ CH6/ch6-2/p3.py
@@ -4,24 +4,24 @@ import numpy as np
 img = cv2.imread("lenna.bmp", cv2.IMREAD_GRAYSCALE)
 if img is None:
     raise Exception("파일 읽기 오류")
+
 rows, cols = img.shape
-dst = np.zeros((rows, cols), dtype=np.uint8)
+dst = np.zeros((rows, cols),np.uint8)
+
 for i in range(rows):
     for j in range(cols):
-        x = img[i, j]
-        if x < 128:
+        x = int(img[i, j])
+
+        if (x < 128):
             y = (78 / 128) * x + 50
-        elif x < 200:
-            y = (127 / 72) * (x - 128) + 128
         else:
-            y = 255
-        if y > 255:
+            y = (72 / 127) * (x - 128) + 128
+
+        if (y > 255):
             y = 255
         elif y < 0:
             y = 0
-
         dst[i, j] = int(y)
-
 cv2.imshow("input", img)
 cv2.imshow("output", dst)
 
